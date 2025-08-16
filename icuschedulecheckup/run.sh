822316e79751e8725a10429223253d629d672735
Pavel Dusek <pavel.dusek@gmail.com>
2025-08-16 02:46:21 +0200

    example run

---
+++ icuschedulecheckup/run.sh
@@ -0,0 +1,5 @@
+python3 main.py -p Du -y 2025 -m 9 --rows 3-33 --columns 1-17 --log INFO 2025-09.xls
+
+python3 main.py -p Du -y 2025 -m 9 --rows 3-33 --columns 1-17 --log WARNING 2025-09.xls
+
+python3 main.py -p Du -y 2025 -m 9 --rows 3-33 --columns 1-17 --log WARNING 2025-09.xls
